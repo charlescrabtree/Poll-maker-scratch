71ecbcbbc4a8535ccd88a87e7e6a98c30ae26b64
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-08-05 10:35:20 -0700

    added more html and got java started

--- app.js
+++ app.js
@@ -1,6 +1,29 @@
 // import functions and grab DOM elements
 
+const optionAAddButton = document.querySelector('#candidate-one-plus');
+const optionBButton = document.querySelector('#candidate-two-plus');
+const optionAUndoButton = document.querySelector('#candidate-one-minus');
+const optionBUndoButton = document.querySelector('#candidate-two-minus');
+
+const form = document.querySelector('form');
+const closePollButton = document.querySelector('#finish-poll');
+const questionEl = document.querySelector('#poll-question');
+const optionATitleEl = document.querySelector('#option-a-title');
+const optionBTitleEl = document.querySelector('#option-b-title');
+const optionAVotesEl = document.querySelector('#option-a-votes');
+const optionBVotesEl = document.querySelector('#option-b-votes');
+
+const pastPollsEl = document.querySelector('.past-polls');
+
 // let state
+let question = '';
+let optionAVotes = 0;
+let optionBVotes = 0;
+let optionATitle = '';
+let optionBTitle = '';
+
+const pastPollsArray = [];
+
 
 // set event listeners 
   // get user input
